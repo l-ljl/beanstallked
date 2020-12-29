90cc46ad7cca348ebb76c6226580f25826f2d6ce
haojunliang <774426443@qq.com>
2020-12-29 16:15:41 +0800

    创建项目

---
+++ src/main/java/com/huice/beanstallked/BeanstallkedApplication.java
@@ -0,0 +1,13 @@
+package com.huice.beanstallked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanstallkedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanstallkedApplication.class, args);
+    }
+
+}
